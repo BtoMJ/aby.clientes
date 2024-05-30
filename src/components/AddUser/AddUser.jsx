9937c3080a1d944db044585c84211aa72eaf89fe
BtoMJ <bto.mena.17@gmail.com>
2024-05-30 00:47:07 -0600

    Modificación en Agregar Usuario

--- src/components/AddUser/AddUser.jsx
+++ src/components/AddUser/AddUser.jsx
@@ -33,7 +33,7 @@ function AddUser() {
 
     if (user) {
       axios
-        .post("https://abyclientes-production.up.railway.app//api/users/adduser", user)
+        .post("api-clientes-production-140a.up.railway.app/api/users/adduser", user)
         .then((res) => {
           alert("User Added Successfully");
           clearFields();
